f0b4617110acb5c9cc27809c57170fb608683a0f
sanjay <sanjaykumar73189@gmail.com>
2024-10-31 10:41:19 +0530

    added firestore

--- FirebaseContext.tsx
+++ FirebaseContext.tsx
@@ -1,17 +1,21 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"; // Import getStorage
+import dotenv from "dotenv";
+dotenv.config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyARsQMmXd5QM1otCih6j4RexP7u58xJgac",
-  authDomain: "eazyqrauth.firebaseapp.com",
-  projectId: "eazyqrauth",
-  storageBucket: "eazyqrauth.appspot.com",
-  messagingSenderId: "1007245030208",
-  appId: "1:1007245030208:web:60b4a0db3e97b2b9d740a5",
-  measurementId: "G-WN1VQYHRBL",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
 export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
 export const FIREBASE_STORAGE = getFirestore(FIREBASE_APP); // Export storage
+
+// HI
